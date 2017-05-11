e56284091d01073fc068213166f05096b0620f55
bruno.krebs <krebs.bruno@gmail.com>
2017-05-11 17:10:04 -0300

    Load balacing

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Scaling JSF applications with Spring Session
 
-Useful commands:
+## Application commands
 
 ```bash
 # run app locally
@@ -9,15 +9,33 @@ mvn spring-boot:run
 # prepare to dockerize
 mvn clean package
 
-# create Docker image
+# create dockerized app image
 docker build -t spring-session .
 
-# run docker image
-docker run -d -p 8080:8080 --name spring-session spring-session
+# run dockerized app
+docker run -d -p 8081:8080 --name spring-session-1 -e "APPLICATION_TITLE=First instance" spring-session
+docker run -d -p 8082:8080 --name spring-session-2 -e "APPLICATION_TITLE=Second instance" spring-session
 
 # stop and remove docker instance
-docker rm -f spring-session
+docker rm -f spring-session-1
+docker rm -f spring-session-2
 
 # getting application logs
 docker logs spring-session
+```
+
+## NGINX commands
+
+```bash
+# create dockerized app image
+docker build -t spring-session-nginx ./nginx/
+
+# run dockerized app
+docker run -p 8080:80 -d --name spring-session-nginx spring-session-nginx
+
+# stop and remove docker instance
+docker rm -f spring-session-nginx
+
+# getting application logs
+docker logs spring-session-nginx
 ```
--- src/main/java/com/auth0/samples/spring/session/HomeBean.java
+++ src/main/java/com/auth0/samples/spring/session/HomeBean.java
@@ -16,41 +16,48 @@
 
 package com.auth0.samples.spring.session;
 
+import org.primefaces.event.ReorderEvent;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
-import javax.inject.Named;
-
-import lombok.Getter;
-import lombok.Setter;
-import org.primefaces.event.ReorderEvent;
-
-@Setter
-@Getter
 @Named
-@ViewScoped
+@SessionScoped
 public class HomeBean implements Serializable {
+
+	@Value("${application.title}")
+	private String applicationTitle;
 
 	private static final long serialVersionUID = 1L;
 
 	private List<Product> products = new ArrayList<>();
 
 	public HomeBean() {
-		products.add(new Product("Milk", BigDecimal.valueOf(1)));
-		products.add(new Product("Cheese", BigDecimal.valueOf(1.2)));
-		products.add(new Product("Bread", BigDecimal.valueOf(1.1)));
-		products.add(new Product("Pasta", BigDecimal.valueOf(2)));
-		products.add(new Product("Gruyere", BigDecimal.valueOf(5)));
+		products.add(new Product("Product A", BigDecimal.valueOf(1)));
+		products.add(new Product("Product B", BigDecimal.valueOf(2)));
+		products.add(new Product("Product C", BigDecimal.valueOf(3)));
+		products.add(new Product("Product D", BigDecimal.valueOf(4)));
+		products.add(new Product("Product E", BigDecimal.valueOf(5)));
 	}
 
 	public void onRowReorder(ReorderEvent event) {
 		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Row Moved",
-				"From: " + event.getFromIndex() + ", To:" + event.getToIndex());
+				"Request handled by: " + applicationTitle);
 		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public String getApplicationTitle() {
+		return applicationTitle;
 	}
 }
